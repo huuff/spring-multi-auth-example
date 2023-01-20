f4d6ae1c0f6ad661614fc9198c9c4c9303fe450a
Haf <haf@protonmail.ch>
2023-01-20 08:21:58 +0100

    converting roles

--- src/main/kotlin/xyz/haff/auth/config/SecurityConfig.kt
+++ src/main/kotlin/xyz/haff/auth/config/SecurityConfig.kt
@@ -2,16 +2,40 @@ package xyz.haff.auth.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.converter.Converter
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.invoke
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter
 import org.springframework.security.web.server.SecurityWebFilterChain
+import reactor.core.publisher.Flux
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity(proxyTargetClass = true)
 class SecurityConfig {
+
+    @Bean
+    fun jwtAuthenticationConverter(): ReactiveJwtAuthenticationConverter {
+        val grantedAuthoritiesConverter = Converter<Jwt, Flux<GrantedAuthority>> { source ->
+            Flux.concat(
+                Flux.fromIterable(
+                    ((source.claims["realm_access"] as Map<String, Any>)["roles"] as List<String>)
+                ).map { SimpleGrantedAuthority("ROLE_$it") },
+                Flux.fromIterable(
+                    ((source.claims["scope"] as String).split(" "))
+                ).map { SimpleGrantedAuthority("SCOPE_$it") },
+            )
+        }
+
+        val jwtAuthenticationConverter = ReactiveJwtAuthenticationConverter()
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter)
+        return jwtAuthenticationConverter
+    }
 
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
@@ -21,7 +45,9 @@ class SecurityConfig {
             logout { disable() }
             httpBasic { disable() }
             oauth2ResourceServer {
-                jwt {  }
+                jwt {
+                    jwtAuthenticationConverter()
+                }
             }
         }
     }
--- src/main/kotlin/xyz/haff/auth/controller/Entrypoint.kt
+++ src/main/kotlin/xyz/haff/auth/controller/Entrypoint.kt
@@ -2,6 +2,7 @@ package xyz.haff.auth.controller
 
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -23,11 +24,16 @@ class Entrypoint {
     @PreAuthorize("hasAuthority('ROLE_sample-role')")
     @GetMapping("/roled")
     suspend fun roled(): ResponseEntity<String> {
-        return ResponseEntity.ok("User has role 'sample-role'")
+        return ResponseEntity.ok("Client has role 'sample-role'")
     }
 
     @GetMapping("/unauth")
     suspend fun unauth(): ResponseEntity<String> {
         return ResponseEntity.ok("Anybody can access this")
+    }
+
+    @GetMapping("/info")
+    suspend fun info(auth: Authentication): ResponseEntity<String> {
+        return ResponseEntity.ok(auth.toString());
     }
 }
